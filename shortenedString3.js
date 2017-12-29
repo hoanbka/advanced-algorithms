dddaad372aad0102df73892c3850a7ef29912d1e
Hoan Nguyen <nguyenhoan.hut@gmail.com>
2017-12-29 11:15:22 +0700

    shortenedString

--- shortenedString3.js
+++ shortenedString3.js
@@ -1,31 +1,29 @@
 function shortenedString(ss) {
-    let stack = [], temp = '';
-    for (let i = 0; i< ss.length; i+=1) {
-        temp = '';   
-        if(ss[i].match(/\s+/)) {
+    let stack = [],
+        temp = '';
+    for (let i = 0; i < ss.length; i += 1) {
+        temp = '';
+        if (ss[i].match(/\s+/)) {
             continue;
-        }
-        else if(ss[i +1] && ss[i+1].match(/\d+/)) {
+        } else if (ss[i + 1] && ss[i + 1].match(/\d+/)) {
             let pos = i;
-            let repeatAmount = ss[i+1];
-            while(ss[i+2] && ss[i+2].match(/\d+/)){
-                repeatAmount += ss[i+2];
+            let repeatAmount = ss[i + 1];
+            while (ss[i + 2] && ss[i + 2].match(/\d+/)) {
+                repeatAmount += ss[i + 2];
                 i++;
             }
-            if(ss[pos] == ')') {
+            if (ss[pos] == ')') {
                 let openBracket = stack.lastIndexOf('(');
-                temp += stack.slice(openBracket+1).join('');
+                temp += stack.slice(openBracket + 1).join('');
                 stack.splice(openBracket);
-            }
-            else{
+            } else {
                 temp = ss[pos];
             }
-            
+
             temp = temp.repeat(repeatAmount);
             stack.push(temp);
-            i+=1;
-        }
-        else{
+            i += 1;
+        } else {
             stack.push(ss[i]);
         }
     }
