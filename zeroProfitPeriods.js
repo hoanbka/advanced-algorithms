8f0385434c723dde224af0a3b1e4d0252edabaaa
Albert <nguyenhoan.hut@gmail.com>
2018-06-20 21:25:49 +0700

    Update zeroProfitPeriods.js

--- zeroProfitPeriods.js
+++ zeroProfitPeriods.js
@@ -1,54 +1,15 @@
 function zeroProfitPeriods(transactions) {
     let count = 0;
     let sum = 0;
-    let first = transactions[0];
-    let index = 0;
-
+    let map = {};
+    map[0] = 1;
     for (let i = 0; i < transactions.length; i++) {
-        if (transactions[i] === 0) {
-            count++;
-            sum = 0;
-            first = transactions[i + 1];
-            index = i + 1;
-            continue;
-        }
-
         sum += transactions[i];
-        if (sum === 0) {
+        if (map[sum]) {
             count++;
-            first = transactions[i + 1];
-            index = i + 1;
-            continue;
-        }
-        let temp = sum;
-        let tmpIndex = index;
-        let found = false;
-
-        if (sum !== transactions[i]) {
-            if (sum - first === 0) {
-                count++;
-                sum = 0;
-                first = transactions[i + 1];
-                index = i + 1;
-            } else {
-                while (index < i) {
-                    sum -= transactions[index];
-                    index++;
-
-                    if (sum === 0) {
-                        count++;
-                        index = i + 1;
-                        found = true;
-                        break;
-                    }
-                }
-
-                if (!found) {
-                    sum = temp;
-                    index = tmpIndex;
-                }
-            }
-        }
+            map = {};
+            map[sum] = 1;
+        } else map[sum] = 1;
     }
     return count;
 }
