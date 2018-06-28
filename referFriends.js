849dda7ffe847ce82d0fe17235926d8b13e930ac
Albert <nguyenhoan.hut@gmail.com>
2018-06-28 15:32:20 +0700

    Update referFriends.js

--- referFriends.js
+++ referFriends.js
@@ -1,30 +1,30 @@
 function referFriends(str) {
     let arr = JSON.parse(str.replace(/^\\" ]/, ''))
 
-    let mapIdWithUserName = new Map();
-    let referrerIdMap = new Map();
+    let mapIdWithUserName = new Map(),
+        referrerIdMap = new Map()
     arr.map(e => {
-        mapIdWithUserName.set(e['_id'], e['username']);
+        mapIdWithUserName.set(e['_id'], e['username'])
 
         let referrerId = e['referrerId']
         if (referrerId) {
-            if (referrerIdMap.has(referrerId)) referrerIdMap.set(referrerId, referrerIdMap.get(referrerId) + 1);
-            else referrerIdMap.set(referrerId, 1);
+            if (referrerIdMap.has(referrerId)) referrerIdMap.set(referrerId, referrerIdMap.get(referrerId) + 1)
+            else referrerIdMap.set(referrerId, 1)
         }
     })
 
-    let ans = [];
-    let userNames = Array.from(mapIdWithUserName.keys());
+    let ans = [],
+        _Ids = Array.from(mapIdWithUserName.keys())
 
-    userNames.map(e => {
-        let userName = mapIdWithUserName.get(e);
-        let money = referrerIdMap.get(e);
-        let temp = '';
-        if (money) temp = userName + ' $' + money * 500;
-        else temp = userName + ' $0';
+    _Ids.map(e => {
+        let userName = mapIdWithUserName.get(e),
+         money = referrerIdMap.get(e),
+         temp = '';
+        if (money) temp = userName + ' $' + money * 500
+        else temp = userName + ' $0'
 
         ans.push(temp)
     })
 
-    return ans.sort();
+    return ans.sort()
 }
