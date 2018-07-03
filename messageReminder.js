fa3526d70a270089af3b3d35fdad8f9ecc21a9d0
Albert <nguyenhoan.hut@gmail.com>
2018-07-03 09:03:16 +0700

    messageReminder

---
+++ messageReminder.js
@@ -0,0 +1,37 @@
+function messageReminder(sender, receiver, date) {
+    let ans = [];
+    let hash = {};
+
+    date.forEach((d, i) => {
+        hash[d] = sender[i] + '-' + receiver[i]
+    })
+
+    let pairs = Object.keys(hash);
+    pairs.sort();
+
+    for (let i = 0; i < date.length; i++) {
+        let message = messageHandler(date[i]);
+        let check = false;
+
+        for (let j = i + 1; j < pairs.length; j++) {
+            if (message >= pairs[j] && hash[date[i]] === receiver[j] + '-' + sender[j]) {
+                check = true;
+            }
+        }
+
+        if (!check) {
+            message += hash[date[i]].split('-')[1] + ' please respond to ' + hash[date[i]].split('-')[0];
+            ans.push(message)
+        }
+    }
+    return ans.sort();
+}
+
+function replace(str, index, chr) {
+    if (index > str.length - 1) return str;
+    return str.substr(0, index) + chr + str.substr(index + 1);
+}
+
+function messageHandler(s) {
+    return new Date(new Date(s).getTime() + 48 * 60 * 60 * 1000).toISOString().replace(/T|000Z/g,' ');
+}
