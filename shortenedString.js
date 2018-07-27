e3efaab10ccd52e63faed5f8866d430075c353d2
Hoan Nguyen Van <nguyenhoan.hut@gmail.com>
2018-07-27 13:33:25 +0700

    shortened string

---
+++ shortenedString.js
@@ -0,0 +1,32 @@
+function shortenedString(ss) {
+    let stack = [],
+        temp = '';
+        
+    for (let i = 0; i < ss.length; i += 1) {
+        temp = '';
+        if (ss[i].match(/\s+/)) {
+            continue;
+        } else if (ss[i + 1] && ss[i + 1].match(/\d+/)) {
+            let pos = i;
+            let repeatAmount = ss[i + 1];
+            while (ss[i + 2] && ss[i + 2].match(/\d+/)) {
+                repeatAmount += ss[i + 2];
+                i++;
+            }
+            if (ss[pos] == ')') {
+                let openBracket = stack.lastIndexOf('(');
+                temp += stack.slice(openBracket + 1).join('');
+                stack.splice(openBracket);
+            } else {
+                temp = ss[pos];
+            }
+
+            temp = temp.repeat(repeatAmount);
+            stack.push(temp);
+            i += 1;
+        } else {
+            stack.push(ss[i]);
+        }
+    }
+    return stack.join('');
+}
