60850ee1c971c358e64b94cd9639680793e09fb5
Hoan Nguyen <nguyenhoan.hut@gmail.com>
2018-08-10 21:34:11 +0700

    frisbees.js
    You and your friends are seizing another summer day, passing around the frisbee in the park. You have the most fun when the game is fair, so as much as possible, you would all like to ensure that everyone receives the disc an equal number of times. You also like to challenge yourselves, so when possible, you'll go for the longest pass you can throw.
    
    So each friend will throw according to the following rules:
    
    Pass to the friend who has held the frisbee the least amount of times.
    In the event of a tie, pass to the furthest among these friends.
    If there's also a tie for furthest, pass to the one of these friends with the lowest index.
    Given an array friends containing info about each player (where they're standing and how far they can throw), as well as the index of the starting player and the total number of passes, your task is to find which player will be holding the frisbee after all the passes are complete.
    
    For friends = [[152, 213, 276], [274, 259, 151], [40, 57, 130], [203, 87, 189], [43, 182, 163]], numberOfPasses = 19, and startingPlayer = 4, the output should be frisbees(friends, numberOfPasses, startingPlayer) = 4.

---
+++ frisbees.js
@@ -0,0 +1,87 @@
+function frisbees(friends, numberOfPasses, startingPlayer) {
+    // an array to keep track the held times of each player
+    let arr = new Array(friends.length).fill(0);
+    arr[startingPlayer] = 1;
+    // calculate an array of distances of each player
+    // that he CAN throw a disc (throwingRange >= distance btw 2 players)
+    // distance = ((x1 - x2)**2 + (y1 - y2)**2)**.5
+    // we also can compare : throwingRange**2 >= (x1 - x2)**2 + (y1 - y2)**2
+    let hash = {};
+
+    for (let i = 0; i < friends.length; i++) {
+
+        let temp = [];
+        let indices = [];
+
+        for (let j = 0; j < friends.length; j++) {
+            if (i == j) continue;
+            let distance = (friends[i][0] - friends[j][0]) ** 2 + (friends[i][1] - friends[j][1]) ** 2
+            if (friends[i][2] ** 2 >= distance) {
+                temp.push([distance, j])
+                indices.push(j);
+            }
+        }
+
+        hash[i] = { 'distance': temp, 'indices': indices };
+    }
+
+    // total of passes so far
+    let soFarPasses = 0;
+
+    // keep track the current player
+    let currPlayerIndex = startingPlayer;
+
+    while (soFarPasses < numberOfPasses) {
+
+        // find the next player to pass a disc
+        // then update currPlayer, increase soFarPasses
+        let indices = hash[currPlayerIndex].indices;
+        let distances = hash[currPlayerIndex].distance;
+        let obj = {};
+
+        distances.map(e => obj[e[1]] = e[0]);
+
+        let expected = {
+            heldTimes: arr[indices[0]],
+            maxDis: obj[indices[0]],
+            index: indices[0]
+        }
+
+        for (let k = 1; k < indices.length; k++) {
+
+            // compare heldTimes
+            if (arr[indices[k]] < expected.heldTimes) {
+                expected.heldTimes = arr[indices[k]];
+                expected.maxDis = obj[indices[k]];
+                expected.index = indices[k];
+
+            } else if (arr[indices[k]] == expected.heldTimes) {
+
+                // compare distances
+                if (expected.maxDis > obj[indices[k]]) continue;
+
+                else if (expected.maxDis < obj[indices[k]]) {
+                    expected.heldTimes = arr[indices[k]];
+                    expected.maxDis = obj[indices[k]];
+                    expected.index = indices[k];
+
+                } else {
+                    // compare indices
+                    if (expected.index < indices[k]) continue;
+                    else {
+                        expected.heldTimes = arr[indices[k]];
+                        expected.maxDis = obj[indices[k]];
+                        expected.index = indices[k];
+                    }
+                }
+            }
+        }
+
+        // update currPlayerIndex
+        currPlayerIndex = expected.index;
+        arr[currPlayerIndex]++;
+        soFarPasses++;
+    }
+
+    return currPlayerIndex;
+}
