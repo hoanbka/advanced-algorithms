8de134aefb3a195a160843adfd6484f787f3589b
Hoan Nguyen <nguyenhoan.hut@gmail.com>
2018-10-07 15:11:13 +0700

    bestSeat.js

--- bestSeat.js
+++ bestSeat.js
@@ -2,23 +2,23 @@ function bestSeat(classLayout) {
     if (classLayout.length == 0) return [-1, -1]
     let ans = [];
     let max = undefined;
+    const len = classLayout.length;
 
-    for (let i = 0; i < classLayout.length; i++) {
-        let temp = 0;
-        let bool = false;
-
+    for (let i = 0; i < len; i++) {
         for (let j = 0; j < classLayout[i].length; j++) {
+            let temp = 0;
+            let bool = false;
 
             if (classLayout[i][j] == -1) {
                 bool = true;
-                if (i - 1 >= 0 && j - 1 >= 0 && classLayout[i - 1][j - 1] && classLayout[i - 1][j - 1] > 0) temp += classLayout[i - 1][j - 1];
-                if (j - 1 >= 0 && classLayout[i][j - 1] && classLayout[i][j - 1] > 0) temp += classLayout[i][j - 1];
-                if (i + 1 < classLayout.length && j - 1 >= 0 && classLayout[i + 1][j - 1] && classLayout[i + 1][j - 1] > 0) temp += classLayout[i + 1][j - 1];
-                if (i + 1 < classLayout.length && classLayout[i + 1][j] && classLayout[i + 1][j] > 0) temp += classLayout[i + 1][j];
-                if (i + 1 < classLayout.length && j + 1 < classLayout[i].length && classLayout[i + 1][j + 1] && classLayout[i + 1][j + 1] > 0) temp += classLayout[i + 1][j + 1];
-                if (j + 1 < classLayout[i].length && classLayout[i][j + 1] && classLayout[i][j + 1] > 0) temp += classLayout[i][j + 1];
-                if (i - 1 >= 0 && j + 1 < classLayout[i].length && classLayout[i - 1][j + 1] && classLayout[i - 1][j + 1] > 0) temp += classLayout[i - 1][j + 1];
-                if (i - 1 >= 0 && classLayout[i - 1][j] && classLayout[i - 1][j] > 0) temp += classLayout[i - 1][j];
+                if (i - 1 >= 0 && j - 1 >= 0 && classLayout[i - 1][j - 1] > 0) temp += classLayout[i - 1][j - 1];
+                if (j - 1 >= 0 && classLayout[i][j - 1] > 0) temp += classLayout[i][j - 1];
+                if (i + 1 < len && j - 1 >= 0 && classLayout[i + 1][j - 1] > 0) temp += classLayout[i + 1][j - 1];
+                if (i + 1 < len && classLayout[i + 1][j] > 0) temp += classLayout[i + 1][j];
+                if (i + 1 < len && j + 1 < classLayout[i].length && classLayout[i + 1][j + 1] > 0) temp += classLayout[i + 1][j + 1];
+                if (j + 1 < classLayout[i].length && classLayout[i][j + 1] > 0) temp += classLayout[i][j + 1];
+                if (i - 1 >= 0 && j + 1 < classLayout[i].length && classLayout[i - 1][j + 1] > 0) temp += classLayout[i - 1][j + 1];
+                if (i - 1 >= 0 && classLayout[i - 1][j] > 0) temp += classLayout[i - 1][j];
 
             }
 
@@ -36,14 +36,9 @@ function bestSeat(classLayout) {
                     }
                 }
             }
-
-            temp = 0;
-            bool = false;
         }
-
     }
 
-    console.log(ans)
-    if (ans.length == 0) return [-1, -1]
-    return ans[0];
+    if (!ans.length) return [-1, -1]
+    return ans[0]
 }
