29f0133a0e2509f43220d2b2b293df7ad2f325d1
Hoan Nguyen <nguyenhoan.hut@gmail.com>
2018-11-02 10:25:12 +0700

    fragileRotatingBox.js

--- fragileRotatingBox.js
+++ fragileRotatingBox.js
@@ -1,45 +1,44 @@
 function fragileRotatingBox(boxWeakness, surfaceRoughness) {
-    let bottom = boxWeakness[boxWeakness.length-1];
-    let right = '', left = '', top = '';
-    
-    boxWeakness.map(e=>{
-        right += e[e.length-1];
-    })
-    
-    right = right.split('').reverse().join('');
-    top = boxWeakness[0].split('').reverse().join('');
-    boxWeakness.map(e=>{
+    let bottom = boxWeakness[boxWeakness.length - 1];
+    let right = '',
+        left = '',
+        top = '';
+
+    boxWeakness.map(e => {
+        right += e[e.length - 1];
         left += e[0];
     })
-    
-    
+
+    right = right.split('').reverse().join('');
+    top = boxWeakness[0].split('').reverse().join('');
+
     let ans = 0;
     let sides = [bottom, right, top, left];
     let index = 0;
     let rotate = '';
     let isGoing = true;
-    
-    while(isGoing){
-        if((rotate + sides[index]).length <= surfaceRoughness.length){
+
+    while (isGoing) {
+        if ((rotate + sides[index]).length <= surfaceRoughness.length) {
             rotate += sides[index];
-        }else{
-            for(let i = 0;i< sides[index].length;i++){
-                if((rotate + sides[index][i]).length <= surfaceRoughness.length){
+        } else {
+            for (let i = 0; i < sides[index].length; i++) {
+                if ((rotate + sides[index][i]).length <= surfaceRoughness.length) {
                     rotate += sides[index][i];
-                }else{
+                } else {
                     isGoing = false;
                     break;
                 }
             }
         }
-        
+
         index++;
-        if(index == 4) index = 0;
+        if (index == 4) index = 0;
     }
-    
-    for(let i = 0;i< rotate.length;i++){
+
+    for (let i = 0; i < rotate.length; i++) {
         ans += Number(rotate[i]) * Number(surfaceRoughness[i]);
     }
-    
+
     return ans;
 }
