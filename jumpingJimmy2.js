7b22eac0558be2a99aa661c1a3995657a08c7c49
Hoan Nguyen <nguyenhoan.hut@gmail.com>
2018-11-05 08:58:05 +0700

    jumpingJimmy2.js

--- jumpingJimmy2.js
+++ jumpingJimmy2.js
@@ -32,3 +32,45 @@ function jumpingJimmy2(tower, power, poison, jumpHeight) {
 
     return totalHeight;
 }
+
+
+// O(n)
+function jumpingJimmy2(tower, power, poison, jumpHeight) {
+    let totalHeight = 0;
+    let tmp = 0;
+
+    let powerSet = new Set(power);
+    let poisonSet = new Set(poison);
+
+    for (let i = 0; i < tower.length; i++) {
+        if (tmp + tower[i] >= jumpHeight) {
+
+            if (tmp + tower[i] == jumpHeight) {
+                totalHeight += tower[i];
+                tmp = 0;
+
+                if (powerSet.has(i)) jumpHeight += 1
+                else if (poisonSet.has(i)) jumpHeight -= 1
+
+            } else {
+
+                // trace brack index (i-1)
+                if (powerSet.has(i - 1)) jumpHeight += 1
+                else if (poisonSet.has(i - 1)) jumpHeight -= 1
+
+
+                tmp = tower[i];
+                if (tmp > jumpHeight) break;
+
+                totalHeight += tower[i];
+            }
+
+        } else {
+            tmp += tower[i];
+            totalHeight += tower[i];
+        }
+
+    }
+
+    return totalHeight;
+}
