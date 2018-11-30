68bcb55f084b2b3818c434e841af2a57318640f8
Hoan Nguyen <nguyenhoan.hut@gmail.com>
2018-11-30 14:30:25 +0700

    betterOrderTraversal.js

--- betterOrderTraversal.js
+++ betterOrderTraversal.js
@@ -1,4 +1,5 @@
-//
+// Hello from Vietnam :D, Hoan Nguyen Van, Software Engineer
+
 // Definition for binary tree:
 // function Tree(x) {
 //   this.value = x;
@@ -6,72 +7,33 @@
 //   this.right = null;
 // }
 function betterOrderTraversal(root) {
-    var inOrder = function(root) {
-        let result = [];
+    let inOrderRes = [],
+        preOrderRes = [],
+        postOrderRes = [];
+
+    let help = function(root) {
         let node = root;
         let traverse = function(node) {
+            // preOrder traverse
+            preOrderRes.push(node.value);
             node.left && traverse(node.left);
-            result.push(node.value);
+            
+            // inOrder traverse
+            inOrderRes.push(node.value);
             node.right && traverse(node.right);
+            
+            // postOrder traverse
+            postOrderRes.push(node.value);
         };
+        
+        // solve
         traverse(root);
-        return result;
-    };
+        return [inOrderRes, preOrderRes, postOrderRes];
+    }
 
-    var postOrder = function(root) {
-        let result = [];
-        let node = this.root;
-        let traverse = function(node) {
-            node.left && traverse(node.left);
-            node.right && traverse(node.right);
-            result.push(node.value);
-        };
-        traverse(root);
-        return result;
-    };
-
-    var preOrder = function(root) {
-        let result = [];
-        let node = this.root;
-        let traverse = function(node) {
-            result.push(node.value);
-            node.left && traverse(node.left);
-            node.right && traverse(node.right);
-        };
-        traverse(root);
-        return result;
-    };
-
-    console.log(inOrder(root))
-    console.log(preOrder(root))
-    console.log(postOrder(root))
+    let ans = help(root)
+    ans.sort((a,b)=> a[0] - b[0] || a[1] - b[1] || a[2] - b[2])
     
-    if(root == null) return []
-
-    let ans = sort(inOrder(root), preOrder(root), postOrder(root));
-    return ans
+   return ans[0]
+    
 }
-
-function sort(a1, a2, a3){
-       for(let i = 0;i< a1.length;i++){
-           if(a1[i] == a2[i] && a1[i] == a3[i]) continue;
-           if(a1[i] < a2[i]){
-               if(a2[i] <= a3[i]) return a1;
-               if(a1[i] > a3[i]) return a3;
-               if(a1[i] == a3[i]) a2[i+1] = 10**7;
-               else return a1;
-               
-           }else if(a1[i] == a2[i]){
-               if(a1[i] > a3[i]) return a3;
-               else if(a1[i] == a3[i]) continue;
-               else a3[i+1] = 10**7;
-           }else{ // a1 > a2
-               if(a2[i] > a3[i]) return a3;
-               else if(a2[i] == a3[i]) continue;
-               else return a2;
-               
-           }
-       }
-    return a1;
-}
-
