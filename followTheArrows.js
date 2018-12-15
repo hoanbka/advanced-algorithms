7a57c598631d76dfbec41aecc629a7f1027fc24b
Hoan Nguyen <nguyenhoan.hut@gmail.com>
2018-12-15 12:49:47 +0700

    followTheArrows.js

--- followTheArrows.js
+++ followTheArrows.js
@@ -39,12 +39,12 @@ function followTheArrows(directionMap, start) {
 }
 
 // TEST
-let directionMap: 
-["<><<v<<<", 
- "v<<<>>>^", 
- ">v<v^^>>", 
- "^>v<^<^<", 
- "vv>>>^^v"]
+let directionMap = ["<><<v<<<",
+    "v<<<>>>^",
+    ">v<v^^>>",
+    "^>v<^<^<",
+    "vv>>>^^v"
+]
 let start = [1, 2]
 
 /* output
@@ -55,4 +55,33 @@ let start = [1, 2]
  "vv....^v"]
  */
 
-console.log(followTheArrows(directionMap, start))
+
+function followTheArrows(directionMap, start) {
+    let map = directionMap.map(e => e.split(''));
+
+    while (true) {
+
+        let curr = map[start[0]][start[1]];
+        map[start[0]][start[1]] = '.';
+
+        if (curr == '>') {
+            next = [start[0], start[1] + 1];
+        } else if (curr == 'v') {
+            next = [start[0] + 1, start[1]];
+        } else if (curr == '<') {
+            next = [start[0], start[1] - 1];
+        } else if (curr == '^') {
+            next = [start[0] - 1, start[1]];
+        }
+
+        if (map[next[0]] == undefined || map[next[0]][next[1]] == undefined || map[next[0]][next[1]] == '.') {
+            break;
+        }
+
+        // update start
+        start = next;
+    }
+
+    return map.map(e => e.join(''));
+}
+console.log(followTheArrows([">"], [1, 2]))
