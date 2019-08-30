9c60a2d28f05e5a168259130c34fc069a37cecb5
Hoan Nguyen Van <nguyenhoan.hut@gmail.com>
2019-08-30 17:16:34 +0700

    maxSumSegments.js

---
+++ maxSumSegments.js
@@ -0,0 +1,53 @@
+function maxSumSegments(arr) {
+    let cnt = 0;
+    let ans = [];
+    let len = 1;
+
+    while (true) {
+        if (cnt == arr.length) break;
+        let max = -1 * 10 ** 9
+        let temp = []
+        let pos = 0
+        for (let i = 0; i < arr.length; i++) {
+            if (temp.length == len) {
+                if (sum(temp) > max) {
+                    max = sum(temp)
+                    pos = temp[0][1]
+
+                }
+
+                temp.shift()
+                temp.push([arr[i], i])
+                continue
+            }
+
+            temp.push([arr[i], i])
+        }
+
+        if (sum(temp) > max) {
+            pos = temp[0][1]
+        }
+
+        ans.push(pos)
+
+        cnt++
+        len++
+    }
+
+    return ans
+}
+
+function sum(arr) {
+    let s = 0;
+    let tmp = [];
+    for (let i = 0; i < arr.length; i++) {
+        tmp.push(arr[i][0])
+    }
+
+    for (let i = 0; i < tmp.length; i++) {
+        s += tmp[i];
+    }
+
+    return s;
+}
+// console.log(maxSumSegments([-1, 2, 1, 3, -2]))
